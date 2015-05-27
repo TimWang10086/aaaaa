800f746860db095eeb6ecbd1e61c6009d0d69de5
Tim Wang <tim@Tim-3.local>
2015-05-27 17:06:20 +0800

    Initial Commit

---
+++ aaaaa/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  aaaaa
+//
+//  Created by Tim Wang on 15/5/27.
+//  Copyright (c) 2015年 Tim Wang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ aaaaa/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  aaaaa
+//
+//  Created by Tim Wang on 15/5/27.
+//  Copyright (c) 2015年 Tim Wang. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
